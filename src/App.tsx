08814297357a87fd476bfd633d2cc34b4fe7e57d
Zhihai Xiang <16368357+xiangzhihai@users.noreply.github.com>
2024-11-14 22:20:16 -0800

    have basic function fulfilled

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,150 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface FormProps {
+  shouldDisplayBack: boolean;
+  handleBack: (() => void) | null;
+  title: string;
+  buttonText: string;
+  inputValue: string;
+  inputHandler: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleNextStep: () => void;
+}
+
+function Form(formInput: FormProps) {
+  return (
+    <div>
+      {formInput.shouldDisplayBack && (
+        <button onClick={formInput.handleBack!}>Back</button>
+      )}
+      <p>{formInput.title}</p>
+      <input
+        value={formInput.inputValue}
+        onChange={formInput.inputHandler}
+      ></input>
+      <button onClick={formInput.handleNextStep}>{formInput.buttonText}</button>
+    </div>
+  );
+}
+
+enum FormState {
+  NAME = 'Name',
+  EMAIL = 'Email',
+  DOB = 'Date of Birth',
+  PASSWORDS = 'Passwords',
+}
+
+interface handleSubmitProps {
+  name: string;
+  email: string;
+  dob: string;
+  passwords: string;
+}
+
 function App() {
+  const [currentFormState, setCurrentFormState] = useState<FormState>(
+    FormState.NAME
+  );
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [dob, setDob] = useState<string>('');
+  const [passwords, setPaswords] = useState<string>('');
+
+  const handleSubmit = (handleSubmitInput: handleSubmitProps) => {
+    console.log(`Invoking handleSubmitInput with input: ${handleSubmitInput}`);
+    setCurrentFormState(FormState.NAME);
+    setName('');
+    setEmail('');
+    setDob('');
+    setPaswords('');
+  };
+
+  const renderForm = () => {
+    switch (currentFormState) {
+      case FormState.NAME:
+        return (
+          <Form
+            title={FormState.NAME.toString()}
+            buttonText="Next"
+            shouldDisplayBack={false}
+            handleBack={null}
+            inputValue={name}
+            inputHandler={(e) => {
+              setName(e.target.value);
+            }}
+            handleNextStep={() => {
+              setCurrentFormState(FormState.EMAIL);
+            }}
+          ></Form>
+        );
+      case FormState.EMAIL:
+        return (
+          <Form
+            title={FormState.EMAIL.toString()}
+            buttonText="Next"
+            shouldDisplayBack={true}
+            handleBack={() => {
+              setCurrentFormState(FormState.NAME);
+            }}
+            inputValue={email}
+            inputHandler={(e) => {
+              setEmail(e.target.value);
+            }}
+            handleNextStep={() => {
+              setCurrentFormState(FormState.DOB);
+            }}
+          ></Form>
+        );
+      case FormState.DOB:
+        return (
+          <Form
+            title={FormState.DOB.toString()}
+            buttonText="Next"
+            shouldDisplayBack={true}
+            handleBack={() => {
+              setCurrentFormState(FormState.EMAIL);
+            }}
+            inputValue={dob}
+            inputHandler={(e) => {
+              setDob(e.target.value);
+            }}
+            handleNextStep={() => {
+              setCurrentFormState(FormState.PASSWORDS);
+            }}
+          ></Form>
+        );
+      case FormState.PASSWORDS:
+        return (
+          <Form
+            title={FormState.PASSWORDS.toString()}
+            buttonText="Next"
+            shouldDisplayBack={true}
+            handleBack={() => {
+              setCurrentFormState(FormState.DOB);
+            }}
+            inputValue={passwords}
+            inputHandler={(e) => {
+              setPaswords(e.target.value);
+            }}
+            handleNextStep={() => {
+              handleSubmit({
+                name,
+                email,
+                dob,
+                passwords,
+              });
+            }}
+          ></Form>
+        );
+
+      default:
+        return <div>Yo</div>;
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">{renderForm()}</header>
     </div>
   );
 }
